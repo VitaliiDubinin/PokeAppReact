03dcba9be55f721fe7fb5dbf6ce09826e6cc269e
VitaliiDubinin <vitalii.dubinin@edu.bc.fi>
2022-04-25 18:40:26 +0300

    day result 20220425

--- src/components/PokeSingle.js
+++ src/components/PokeSingle.js
@@ -4,9 +4,9 @@ import { Link, Outlet } from "react-router-dom";
 const PokeSingle = () => {
   return (
     <div>
-      Alone PokeMon should be here
-      <Link to="/pokesingle">Show me single pokemon</Link>
-      <Outlet />
+      PokeSingle PokeMon should be here
+      {/* <Link to="/pokesingle">Show me single pokemon</Link>
+      <Outlet /> */}
     </div>
   );
 };
